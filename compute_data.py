6b743b0585ca88d0f5bec78b5c7e654de9ceca04
BorisVV <bcarlof@gmail.com>
2018-02-11 20:48:47 -0600

    Chances to func to reduce code length

--- compute_data.py
+++ compute_data.py
@@ -1,44 +1,102 @@
 import random
-import try_your_luck_game_app
 from try_your_luck_game_app import GAMES_CHOSEN
 
 # Each one gets added one extra number for the range(1, gameNumbs*) *no inclusive.
-POWERBALL, POWERPLAY = 70, 27 #(POWERBALL = 69 + 1)
+POWERBALL, POWERPLAY = 70, 27 #e.g.(POWERBALL uses 69 numbers.)
 GOPHER5 = 48
 NORTHSTAR = 32
 LOTTOAMERICA, LOTAMEPOWERPLAY = 53, 11
 MEGAMILLIONS, MEGAPLAY = 71, 26
 
-def compute_games_chosen(numbOfYears, gamesChosen):
+# Number of times won with 3, 4, etc.
+THREE_NUMBS, FOUR_NUMBS, FIVE_NUMBS, SIX_NUMBS = 0, 0, 0, 0
+
+COUNTER = 0
+QUIT_PICK_FIVE = set()
+
+def _pick_five(game):
+    # Get five numbers from the numbers in a game e.g. Northstar 1-31 add one to 31 for inclusive.
+    # set is working good in this case, but I think list also works.
+    return set(random.sample(range(1, game), 5))
+
+def results_northstar(numbYears, numbQPicks):
+    # If the player choose one quick pick only once for the number of years, the outter loop will
+    # only loop once. Northstar plays everyday.
+
+    global COUNTER, QUIT_PICK_FIVE
+
+    if numbQPicks == 0: # Using the same numbers for the number of years.
+        QUIT_PICK_FIVE = _pick_five(NORTHSTAR) # quick pick for user.
+        print("\nYour Northstar numbers!")
+        numbQPicks = numbYears
+        comp_draws(numbQPicks, NORTHSTAR)
+
+    else:
+        print("\nYour Northstar numbers!")
+        for n in range(numbYears//numbQPicks):
+            QUIT_PICK_FIVE = _pick_five(NORTHSTAR)
+
+            # function to get the draws and check them against the quik pick numbers.
+            comp_draws(numbQPicks, NORTHSTAR)
+
+
+
+def comp_draws(numbOfPicks, gameChosen):
+    global QUIT_PICK_FIVE, COUNTER, THREE_NUMBS, FOUR_NUMBS, FIVE_NUMBS
+    for x in range(numbOfPicks):
+        draws = _pick_five(gameChosen)
+        match = list(draws) # Save a list for printing.
+        draws &= QUIT_PICK_FIVE # kee only the matching numbes <set &= other>.
+        # Check and see if there are matching numbers and keep trak or print.
+        if len(draws) == 3:
+            THREE_NUMBS += 1
+        elif len(draws) == 4:
+            FOUR_NUMBS += 1
+            winnings_print((COUNTER/365), len(draws), sorted(QUIT_PICK_FIVE), sorted(match), sorted(draws))
+        elif len(draws) == 5:
+            THREE_NUMBS += 1
+            winnings_print((COUNTER/365), len(draws), sorted(QUIT_PICK_FIVE), sorted(match), sorted(draws))
+        COUNTER += 1
+    # return COUNTER, THREE_NUMBS, FOUR_NUMBS, FIVE_NUMBS, SIX_NUMBS
+
+def winnings_print(*args):
+    print("\nIt took {:.2f} years to match $$$$$$ {} numbers \n{} <- Your numbers \
+    \n{} <- Computer numbers \n{} Matched \n".format(*args))
+
+def print_results(gamesChosen):
     if 'Northstar' in gamesChosen:
-        _numbOfQuickPicks = (numbOfYears * 365) // 7
-        results_northstar(NORTHSTAR, _numbOfQuickPicks)
+        if FOUR_NUMBS == 0 and FIVE_NUMBS == 0:
+            print("Sorry!! No winnings! with 4 numbers or more.")
 
-    # for game in gamesChosen: # list of games chosen by user.
-    #     if game == 'Northstar': # if in list.
+        print("\nOnly the winnings with 4 or more numbers were shown. \nNorthstar!"\
+            "\n{} times with 3 numbers.\n{} times with 4 numbers.\n{} times with five"\
+             .format(THREE_NUMBS, FOUR_NUMBS, FIVE_NUMBS))
+    elif 'Powerball' in gamesChosen:  # TODO:
+        pass
 
-def results_northstar(game, numbOfQuickPicks):
-    # Northstar draws are everyday, the program has to draw 7 times for each numbOfQuickPicks.
-    northstar3, northstar4, northstar5 = 0, 0, 0
-    numb = 0
-    while numb <= numbOfQuickPicks:
-        numb += 1 # Week 1
+def results_powerball(numbOfQuickPicks):
+    # Powerball only plays twice in a week, so number of draws is 2.
+    powerball4, powerball5, powerball6 = 0, 0, 0
+    count = 1
+    while count <= numbOfQuickPicks:
+        quick_pick_five = set(random.sample(range(1, POWERBALL), 5))
+        quick_pick_one = set(random.sample(range(1, POWERPLAY), 1))
 
-        # set is working good in this case, but I think list also works.
-        quick_pick = set(random.sample(range(1,NORTHSTAR), 5))
+        # Draw twice for each week.
+        thisCount = 0
+        while thisCount < 2:
+            draw_five = set(random.sample(range(1, POWERBALL), 5))
+            draw_one = set(random.sample(range(1, POWERPLAY), 1))
+            draw_five &= quick_pick_five # Find the matches in the fisrt five.
+            draw_one &= quick_pick_one   # See if the one matches.
+            if len(draw_one) > 0:
+                if (len(draw_five) + len(draw_one)) > 2:
+                    print("\nWeek number {}:\n{}{}\n{}{}".format(count, sorted(quick_pick_five),\
+                                                        draw_one, sorted(draw_five), draw_one))
+                    print("Inside the len(draw_one)>0")
+            elif len(draw_five) > 2:
+                print("\nWeek number {}:\n{}{}\n{}".format(count, sorted(quick_pick_five), sorted(draw_five)))
 
-        for n in range(7):
-            draws = set(random.sample(range(1, NORTHSTAR), 5))
-            draws &= quick_pick
-            if len(draws) == 3:
-                northstar3 += 1
-                print("Week number {}\n {}\n {} ***3".format(numb, sorted(quick_pick), sorted(draws)))
-            if len(draws) == 4:
-                northstar4 += 1
-                print("Week number {}\n {}\n {} ****4".format(numb, sorted(quick_pick), sorted(draws)))
-            if len(draws) == 5:
-                northstar5 += 1
-                print("Week number {}\n {}\n {} *****5".format(numb, sorted(quick_pick), sorted(draws)))
-
-    print("\nNorthstar game wins in {:.0f} year: \nWith 3 numbers: {} \nWith 4 number: {}\
-        \nWith 5 numbers: {}".format((numbOfQuickPicks*7/365), northstar3, northstar4, northstar5))
+            thisCount += 1 # Inner loop
+        count += 1 # Outer loop
+    print()
--- try_your_luck_game_app.py
+++ try_your_luck_game_app.py
@@ -18,7 +18,7 @@ def display_games(games):
     for i, v in enumerate(games):
         print(i + 1, v)
 
-def validate_input():
+def validate_integer():
     while True: # Validate the user input
         try:
             usr_int = int(input())
@@ -26,36 +26,48 @@ def validate_input():
         except:
             print("Enter a number:")
 
-def display_options(msg):
+def display_options():
     numb_of_years = 0
     while True:
         display_games(GAMES_OPTIONS)
-        print(msg)
-        game_chosen_numb = validate_input() # func to get int input only.
+        print("\n^ Select one option. Enter the number (You can add more after one selction):")
+        game_chosen_numb = validate_integer() # func to get int input only.
         remove_add_display_game_chosen(game_chosen_numb)
         print("\nYour selection so far:")
         display_games(GAMES_CHOSEN)
-        yes_no = input("\nDo you want to add another game? \ntype:'y' to continue:"\
-                        " or press 'Enter' to Exit:\n")
+        yes_no = input("\nDo you want to add another game? \ntype:'y' to add:"\
+                        " or press 'Enter' to continue:\n")
         if yes_no.lower() != 'y': break
 
 def main():
-    # This is to let the user know what needs to entered.
-    msg_select_one = "\n^ Select one option. Enter the number:"
-
     # Display the games and have the user selec one, etc.
-    display_options(msg_select_one)
+    display_options()
 
     # Shows the user the games chosen to play.
-    print('You selected:')
+    print('\nYou selected:')
     display_games(GAMES_CHOSEN)
+
+    # TODO: The option to for the quick pick numbers is the same for all the games.
+    # I'd like to change it to where the user cand do it for each game.
+    print("\nEnter how many times you want to do quick_picks in a number of year/s that you'd\
+     like to play?\n e.g. \n0 = Same numbers all time, \n1 = everyday, \n7 = everyday seven days(weekly),\
+     \n30 = every month: 'You get the idea :)'")
+    numb_of_qpick = validate_integer()
 
     print("\nEnter the number of years(whole number only) you'd like to try:")
     # Get number of years.
-    numb_of_years = validate_input()
+    numb_of_years = validate_integer()
 
-    # Let the luck begin..
-    compute_data.compute_games_chosen(numb_of_years, GAMES_CHOSEN)
+    numb_of_days = numb_of_years * 365 # Need days to compute results.
+
+    # Compute data.
+    if "Powerball" in GAMES_CHOSEN: compute_data.results_powerball(numb_of_days, numb_of_qpick)
+    if "Northstar" in GAMES_CHOSEN: compute_data.results_northstar(numb_of_days, numb_of_qpick)
+
+    # Display results.
+    print("\nHere are the results in the {} year/s you entered:".format(numb_of_years))
+
+    compute_data.print_results(GAMES_CHOSEN)
 
 if __name__ == "__main__":
     main()
