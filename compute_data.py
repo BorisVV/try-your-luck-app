28b43c88a00fb84419d0a195da52be4b0164238f
BorisVV <bcarlof@gmail.com>
2018-02-13 11:09:33 -0600

    added a select all option

--- compute_data.py
+++ compute_data.py
@@ -1,5 +1,4 @@
 import random
-from try_your_luck_game_app import GAMES_CHOSEN
 
 # Each one gets added one extra number for the range(1, gameNumbs*) *no inclusive.
 POWERBALL, POWERPLAY = 70, 27 #e.g.(POWERBALL uses 69 numbers.)
@@ -10,7 +9,7 @@ MEGAMILLIONS, MEGABALL = 71, 26
 LUCKYFORLIFE, LUCKYBALL = 49, 19
 
 # Number of times won with 3, 4, etc.
-THREE_NUMBS, FOUR_NUMBS, FIVE_NUMBS, SIX_NUMBS = 0, 0, 0, 0
+ONE_NUMB, TWO_NUMBS, THREE_NUMBS, FOUR_NUMBS, FIVE_NUMBS, SIX_NUMBS = 0, 0, 0, 0, 0, 0
 
 COUNTER = 0
 QUIT_PICK_FIVE = set()
@@ -36,18 +35,23 @@ def _wins_six_numbs_games(*sixNumbs):
                 "\n{} {}   <--Comp numbers"\
                 "\n{} {}   <--Matching numbers".format(*sixNumbs))
 
-def display_results(name):
-    print("\tHere are the results for {}"\
+def display_results(years, name):
+    print("\tHere are the numbers of times each match ocurred for"\
+    "\n {} year/s for {}"\
+    "\nThere were {} times with 1 numbers."\
+    "\nThere were {} times with 2 numbers."\
     "\nThere were {} times with 3 numbers."\
     "\nThere were {} times with 4 numbers."\
     "\nThere were {} times with 5 numbers."\
-    .format(name, THREE_NUMBS, FOUR_NUMBS, FIVE_NUMBS))
+    .format(years, name, ONE_NUMB, TWO_NUMBS, THREE_NUMBS, FOUR_NUMBS, FIVE_NUMBS))
 
     if name == "Powerball" or name == "MegaMillions" or name == "LuckyForLife" or name == "LottoAmerica":
         print("There were {} times with 6 numbers.".format(SIX_NUMBS))
 
 def comp_draws(name, numbOfPicks, numbOfDays, drawFive, drawOne=None):
-    global QUIT_PICK_FIVE, QUIT_PICK_ONE, COUNTER, THREE_NUMBS, FOUR_NUMBS, FIVE_NUMBS, SIX_NUMBS
+    global ONE_NUMB, TWO_NUMBS, THREE_NUMBS, FOUR_NUMBS, FIVE_NUMBS, SIX_NUMBS,\
+           QUIT_PICK_FIVE, QUIT_PICK_ONE, COUNTER
+
     if numbOfPicks == 0:
         numbOfPicks = numbOfDays
 
@@ -72,25 +76,31 @@ def comp_draws(name, numbOfPicks, numbOfDays, drawFive, drawOne=None):
                 draw_one &= QUIT_PICK_ONE # See if it matches.
                 numbOfMatches = len(draws) + len(draw_one)
 
-                if numbOfMatches == 3: THREE_NUMBS += 1
+                if numbOfMatches == 1: ONE_NUMB += 1
+                elif numbOfMatches == 2: TWO_NUMBS += 1
+                elif numbOfMatches == 3: THREE_NUMBS += 1
                 if numbOfMatches > 3:
-                    years = COUNTER / 365
-                    if numbOfMatches == 4:
-                        FOUR_NUMBS += 1
-                        _wins_six_numbs_games(years, numbOfMatches, sorted(QUIT_PICK_FIVE),\
-                         QUIT_PICK_ONE, sorted(all_numbs), quick_one, sorted(draws), draw_one)
-                    if numbOfMatches == 5:
-                        FIVE_NUMBS += 1
-                        _wins_six_numbs_games(years, numbOfMatches, sorted(QUIT_PICK_FIVE),\
-                         QUIT_PICK_ONE,sorted(all_numbs), quick_one, sorted(draws), draw_one)
-                    if numbOfMatches == 6:
-                        SIX_NUMBS += 1
-                        _wins_six_numbs_games(years, numbOfMatches, sorted(QUIT_PICK_FIVE),\
-                        QUIT_PICK_ONE, sorted(all_numbs), quick_one, sorted(draws), draw_one)
+                    if len(draw_one) == 0:
+                        draw_one = 0
+                elif numbOfMatches == 4:
+                    FOUR_NUMBS += 1
+                    _wins_six_numbs_games((COUNTER/365), numbOfMatches, sorted(QUIT_PICK_FIVE),\
+                     QUIT_PICK_ONE, sorted(all_numbs), quick_one, sorted(draws), draw_one)
+                elif numbOfMatches == 5:
+                    FIVE_NUMBS += 1
+                    _wins_six_numbs_games((COUNTER/365), numbOfMatches, sorted(QUIT_PICK_FIVE),\
+                     QUIT_PICK_ONE,sorted(all_numbs), quick_one, sorted(draws), draw_one)
+                elif numbOfMatches == 6:
+                    SIX_NUMBS += 1
+                    _wins_six_numbs_games((COUNTER/365), numbOfMatches, sorted(QUIT_PICK_FIVE),\
+                    QUIT_PICK_ONE, sorted(all_numbs), quick_one, sorted(draws), draw_one)
 
             else:
                 numbOfMatches = len(draws)
-                if numbOfMatches == 3:
+                if numbOfMatches == 1: ONE_NUMB += 1
+                elif numbOfMatches == 2: TWO_NUMBS += 1
+                elif numbOfMatches == 3: THREE_NUMBS += 1
+                elif numbOfMatches == 3:
                     THREE_NUMBS += 1
                 elif numbOfMatches == 4:
                     FOUR_NUMBS += 1
@@ -98,11 +108,11 @@ def comp_draws(name, numbOfPicks, numbOfDays, drawFive, drawOne=None):
                 elif numbOfMatches == 5:
                     THREE_NUMBS += 1
                     _wins_five_numbs_games((COUNTER/365), numbOfMatches, sorted(QUIT_PICK_FIVE), sorted(all_numbs), sorted(draws))
-
             COUNTER += 1
     if FOUR_NUMBS == 0 and FIVE_NUMBS == 0 and SIX_NUMBS == 0:
         print("***No wins with 4 numbers or more.***")
 
-    display_results(name)
+    display_results(int(numbOfDays/365), name)
 
-    COUNTER, QUIT_PICK_FIVE, QUIT_PICK_ONE, THREE_NUMBS, FOUR_NUMBS, FIVE_NUMBS, SIX_NUMBS = 0, 0, 0, 0, 0, 0, 0
+    ONE_NUMB, TWO_NUMBS, THREE_NUMBS, FOUR_NUMBS, FIVE_NUMBS, SIX_NUMBS = 0, 0, 0, 0, 0, 0
+    COUNTER, QUIT_PICK_FIVE, QUIT_PICK_ONE = 0, 0, 0
--- try_your_luck_game_app.py
+++ try_your_luck_game_app.py
@@ -1,14 +1,11 @@
 import compute_data
 
 GAMES_OPTIONS = ["Powerball", "MegaMillions", "Northstar", "GopherFive",\
-"LottoAmerica", "LuckyForLife"]
+"LottoAmerica", "LuckyForLife", "Select All"]
 
 GAMES_CHOSEN = [] # Every time the user selects one, it gets added to this list.
 
 def remove_add_display_game_chosen(numbOfGameChosen):
-    if numbOfGameChosen > len(GAMES_OPTIONS) or numbOfGameChosen < 1:
-        print("Oops! Number not in list.")
-        return
     for index, val in enumerate(GAMES_OPTIONS):
         if index + 1 == numbOfGameChosen:
             GAMES_CHOSEN.append(val)
@@ -27,31 +24,46 @@ def validate_integer():
             print("Enter a number:")
 
 def display_options():
-    numb_of_years = 0
+
+    display_games(GAMES_OPTIONS)
     while True:
-        display_games(GAMES_OPTIONS)
-        print("\n^ Select one option. Enter the number (You can add more after one selction):")
+        # Display options and loop to get user's chosen ones.
+        print("^  Select one option. Type the number (You can add more after one selection)"\
+            "\nIf you want to select the rest or all of the other games at the same time"\
+            "\nType the number for Select All:")
         game_chosen_numb = validate_integer() # func to get int input only.
-        remove_add_display_game_chosen(game_chosen_numb)
-        print("\nYour selection so far:")
-        display_games(GAMES_CHOSEN)
-        yes_no = input("\nDo you want to add another game? \ntype:'y' to add:"\
-                        " or press 'Enter' to continue:\n")
-        if yes_no.lower() != 'y': break
+        # Any number that is less or greater than the length of GAMES_OPTIONS.
+        if game_chosen_numb > len(GAMES_OPTIONS) or game_chosen_numb < 1:
+            print("Oops! Number not in list.")
+            continue
+        # If the selection is the last option, which is select all.
+        if game_chosen_numb == len(GAMES_OPTIONS):
+            global GAMES_CHOSEN
+            [GAMES_CHOSEN.append(game) for game in GAMES_OPTIONS[:-1]]
+            break
+        # If the above condition are false then.
+        else:
+            remove_add_display_game_chosen(game_chosen_numb)
+            print("\tYour selection so far:")
+            display_games(GAMES_CHOSEN)
+            print("\tHere are the other options.")
+            display_games(GAMES_OPTIONS)
+            yes_no = input("\tType:'y/Yes' to add more, else, press 'ENTER' to continue:\n")
+            if yes_no.lower() != 'y' or yes_no.lower() != 'yes': continue
 
 def main():
     # Display the games and have the user selec one, etc.
     display_options()
 
     # Shows the user the games chosen to play.
-    print('\nYou selected:')
+    print('\n**You selected:')
     display_games(GAMES_CHOSEN)
 
     # TODO: The option to for the quick pick numbers is the same for all the games.
     # I'd like to change it to where the user cand do it for each game.
-    print("\nEnter how many times you want to do quick_picks in a number of year/s that you'd"\
-        "like to play?\n e.g. \n0 = Same numbers all time, \n1 = everyday, \n7 = everyday seven days(weekly),\
-        \n30 = every month: 'You get the idea :)'")
+    print("\nHow many times do you want to do quick_picks in a year/s? \n e.g."\
+        "\n0 = Same numbers all time, \n1 = everyday, \n7 = everyday seven days(weekly),"\
+        "\n30 = every month: 'You get the idea :)'")
     numb_of_qpick = validate_integer()
 
     print("\nEnter the number of years(whole number only) you'd like to try:")
@@ -60,7 +72,7 @@ def main():
 
     numb_of_days = numb_of_years * 365 # Need days to compute results.
     print("# " * 40)
-    print("Number or years {} \nQuick pick {} day/s".format(numb_of_years, numb_of_qpick))
+    print("Number or years {} \nYour quick pick is 1 every {} day/s".format(numb_of_years, numb_of_qpick))
 
     # Compute data.
     if "Northstar" in GAMES_CHOSEN:
