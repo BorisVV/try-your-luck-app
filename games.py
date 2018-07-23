0cb053e6e8b7b78ac9ffe8586bd7eb2c63046a43
BorisVV <bcarlof@gmail.com>
2018-07-23 16:26:53 -0500

    modified files to ADD MORE READALITY.

--- games.py
+++ games.py
@@ -55,5 +55,31 @@ def display_options():
             game_chosen_add_and_remove(numb_selected)
             print("-" * 50, "\n\tYour selection so far:")
             display_games(games_selected)
-            yes_no = input("Type: 'y/Yes' to add more, else, press 'ENTER' to continue:\n")
+            yes_no = input("Type: 'y/Yes' to add more, else, press 'ENTER' or any other key to continue:\n")
             if yes_no.lower() != 'y' and yes_no.lower() != 'yes': break
+
+def getNumberOfYears():
+    # User needs to enter a whole number, between the range set.
+    while True:
+        print("Press 'Control ^ z' to Exit or"\
+              "\nEnter the number of years(whole numbers only) that you'd like to try:")
+        # Get number of years.
+        numb_of_years = validate_integer()
+        if numb_of_years < 1 or numb_of_years > 30:
+            print("---Error-Lets keep the number of years between 1 and 30")
+            continue
+        else:
+            return numb_of_years
+
+def getNumbOfQuickPicks(numb_of_years):
+    while True:
+        # Games are played different, like once a day or twice a week, etc.
+        print("\nHow many times do you want to do quick_picks in the {} year/s that you entered?"\
+        "\n1 = Same quick pick all time?, \n7 = weekly?, \n14 = every two weeks?,"\
+        "\n21 = every three weeks?, or \n28 = every month?:".format(numb_of_years))
+        numb_of_qpick = validate_integer()
+        if numb_of_qpick != 1 and numb_of_qpick != 7 and numb_of_qpick != 14\
+                and numb_of_qpick != 21 and numb_of_qpick != 28:
+            print("\n Oops! Enter one number in options only. try again!")
+        else:
+            return numb_of_qpick
--- play.py
+++ play.py
@@ -1,53 +1,40 @@
-import games as g
+import games as _g
+import compute_data as _c_d
 
 def main():
     '''Display instructions to select games then enter years followed by how many times'''
 
-    g.display_options()
+    _g.display_options()
 
-    # User needs to enter a whole number, between the range set.
-    while True:
-        print("Press 'Control ^ z' to Exit or"\
-              "\nEnter the number of years(whole number only) that you'd like to try:")
-        # Get number of years.
-        numb_of_years = g.validate_integer()
-        if numb_of_years < 1 or numb_of_years > 30:
-            print("---Error-Lets keep the number of years between 1 and 30")
-            continue
-        else: break
+    # This will return the number of years the user wants to play the numbers.
+    numb_of_years = _g.getNumberOfYears()
 
     # Total number of days in the number of years enteed by user.
     numb_of_days = numb_of_years * 365 # Need days to compute results.
 
-    while True:
-        # Games are played different, like once a day or twice a week, etc.
-        print("\nHow many times do you want to do quick_picks in the {} year/s you entered?"\
-        "\n1 = Same quick pick all time?, \n7 = weekly?, \n14 = every two weeks?,"\
-        "\n21 = every three weeks?, or \n28 = every month?:".format(numb_of_years))
-        numb_of_qpick = validate_integer()
-        if numb_of_qpick != 1 and numb_of_qpick != 7 and numb_of_qpick != 14\
-                and numb_of_qpick != 21 and numb_of_qpick != 28:
-            print("\n Oops! Enter one number in options only. try again!")
-        else: break
+    # Get the quickpicks selection, it can be weekly, every two weeks, etc..
+    numb_of_qpick = _g.getNumbOfQuickPicks(numb_of_years)
 
-    print("# " * 40)
-    print("Number or years {} \nYour quick pick is 1 every {} day/s \n----------------------\n".format(numb_of_years, numb_of_qpick))
+    print("\n" + "# " * 40)
+    print("Number or years {} \nYour quick pick is 1 ticket every {} day/s \n----------------------\n".format(numb_of_years, numb_of_qpick))
+
+
 
     # Compute data.
-    if "Northstar" in games_selected:
-        compute_data.comp_draws("Northstar", numb_of_qpick, numb_of_days, compute_data.NORTHSTAR, 7)
-    if "GopherFive" in games_selected:
-        compute_data.comp_draws("GopherFive", numb_of_qpick, numb_of_days, compute_data.GOPHER5, 3)
-    if "Powerball" in games_selected:
-        compute_data.comp_draws("Powerball", numb_of_qpick, numb_of_days, compute_data.POWERBALL, 2, compute_data.POWERPLAY)
-    if "MegaMillions" in games_selected:
-        compute_data.comp_draws("MegaMillions", numb_of_qpick, numb_of_days, compute_data.MEGAMILLIONS, 2, compute_data.MEGABALL)
-    if "LottoAmerica" in games_selected:
-        compute_data.comp_draws("LottoAmerica", numb_of_qpick, numb_of_days, compute_data.LOTTOAMERICA, 2, compute_data.LOTAMEPOWERPLAY)
-    if "LuckyForLife" in games_selected:
-        compute_data.comp_draws("LuckyForLife", numb_of_qpick, numb_of_days, compute_data.LUCKYFORLIFE, 2, compute_data.LUCKYBALL)
+    if "North Star" in _g.games_selected:
+        _c_d.comp_draws("Northstar", numb_of_qpick, numb_of_days, _c_d.NORTHSTAR, 7)
+    elif "Gopher Five" in _g.games_selected:
+        _c_d.comp_draws("GopherFive", numb_of_qpick, numb_of_days, _c_d.GOPHER5, 3)
+    elif "Power Ball" in _g.games_selected:
+        _c_d.comp_draws("Powerball", numb_of_qpick, numb_of_days, _c_d.POWERBALL, 2, _c_d.POWERPLAY)
+    elif "Mega Millions" in _g.games_selected:
+        _c_d.comp_draws("MegaMillions", numb_of_qpick, numb_of_days, _c_d.MEGAMILLIONS, 2, _c_d.MEGABALL)
+    elif "Lotto America" in _g.games_selected:
+        _c_d.comp_draws("LottoAmerica", numb_of_qpick, numb_of_days, _c_d.LOTTOAMERICA, 2, _c_d.LOTAMEPOWERPLAY)
+    elif "Lucky For Life" in _g.games_selected:
+        _c_d.comp_draws("LuckyForLife", numb_of_qpick, numb_of_days, _c_d.LUCKYFORLIFE, 2, _c_d.LUCKYBALL)
 
-    compute_data.totalFourOrPlus()
+    _c_d.totalFourOrPlus()
 
 if __name__ == "__main__":
     main()
